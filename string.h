c8f93d09258c6e66d0954798b1b4928e1f97cd69
MatthewDeKoning <matthew.dekoning@okstate.edu>
2017-01-31 13:37:44 -0600

    added comments

--- string.h
+++ string.h
@@ -9,16 +9,34 @@
 //macro to translate the value of a number char to an int
 #define charToInt(c) ((int)c-48)
 
+/* compare two strings:
+   returns 1 if the first string is "greater"
+   returns 0 if they are equal
+   returns -1 if the second string is "greater"
+*/
 int8_t strcomp(const char*, const char*);
 
+/* return the length of a string */
 uint16_t LengthOfString(const char*);
 
+/* fill a char array[ROWS][COLS] with the different parts of 
+   the passed string, broken into parts deliminated by 
+   the last string parameter
+*/
 void strtok(const char*, char[ROWS][COLS], const char*);
 
-int8_t firstIndex(const char* str, const char* delim);
+/* return the index of the first occurence of the passed parameter */
+int8_t firstIndex(const char* , const char* );
 
+/*return the int equal to the value of the number string*/
 int16_t strToInt(const char*);
 
+/*return a string loaded with a numerical value:
+   param num - number to turn into string
+   param str - string to fill with the number
+   param base - base of the number to turn into string 
+   param offset - offset into the string to place the number
+*/
 char* itoa(int , char* , int , int);
 
 #endif //String
